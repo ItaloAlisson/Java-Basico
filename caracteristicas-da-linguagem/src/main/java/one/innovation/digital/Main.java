664bd24f484d204157d75f4d9efb70d19d809a03
ItaloAlisson <italoalisson15@gmail.com>
2023-01-27 19:20:14 -0300

    projetos formatação

---
+++ caracteristicas-da-linguagem/src/main/java/one/innovation/digital/Main.java
@@ -0,0 +1,18 @@
+package one.innovation.digital;
+
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        System.out.println("1234 asda qw".replaceAll("[0-9]" ,""));
+
+//        IntStream.of(1, 2, 3, 4, 5).forEach(n->{
+//            System.out.println(n);
+//        });
+
+//        IntStream.range(0,3).forEach(n->{
+//            System.out.println("Número = "+n);
+//        });
+    }                                                             
+}
